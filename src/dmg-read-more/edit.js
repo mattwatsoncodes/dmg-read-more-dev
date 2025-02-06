f267f86e8e44953b8d8a324c3ca102d064042962
Matt Watson <matt@mattwatson.codes>
2025-02-06 22:20:31 +0000

    Fetch latest posts for block editor: Retrieve the latest 10 published posts in descending order by date using useSelect to enable dynamic post selection in the Gutenberg block.

--- src/dmg-read-more/edit.js
+++ src/dmg-read-more/edit.js
@@ -1,8 +1,29 @@
 import { useBlockProps } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 export default function Edit() {
 	const blockProps = useBlockProps();
+
+	const posts = useSelect( ( select ) => {
+		const query = {
+			per_page: 10,
+			page: 1,
+			orderby: 'date',
+			order: 'desc',
+			status: 'publish',
+		};
+
+		const posts = select( 'core' ).getEntityRecords(
+			'postType',
+			'post',
+			query
+		);
+
+		return posts || [];
+	}, [] );
+
+	console.log( posts );
 
 	const selectedPost = {
 		link: 'http://test.com',
