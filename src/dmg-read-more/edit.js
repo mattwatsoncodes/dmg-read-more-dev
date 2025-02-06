29d59d9e0787a9c5e309db671187ab0cf37b2983
Matt Watson <matt@mattwatson.codes>
2025-02-06 21:11:33 +0000

    Scaffold initial plugin edit file
    Sets up the base file for handling block functionality and removes styles from save so block can build.

--- src/dmg-read-more/edit.js
+++ src/dmg-read-more/edit.js
@@ -1,38 +1,21 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
+import { useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
-
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './editor.scss';
-
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
 export default function Edit() {
+	const blockProps = useBlockProps();
+
+	const selectedPost = {
+		link: 'http://test.com',
+		title: {
+			rendered: __( 'Test Post', 'dmg-read-more' ),
+		},
+	};
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Dmg Read More – hello from the editor!', 'dmg-read-more' ) }
-		</p>
+		<div { ...blockProps }>
+			<p className="dmg-read-more">
+				<a href={ selectedPost.link }>{ __( 'Read More:', 'dmg-read-more' ) } { selectedPost.title.rendered }</a>
+			</p>
+		</div>
 	);
 }
